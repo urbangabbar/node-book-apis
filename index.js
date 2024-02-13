99e1e3e993282f44ef98e3ce099938f58862f2c4
Abhinav Personal <abhinavpersonal@Abhinavs-MacBook-Pro.local>
2024-02-13 21:52:06 +0530

    Phase -2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express') // express module import
+const bodyParser = require('body-parser')
 
 // we called express function to create app object
 const app = express()
 
-// created a get api with url /api/book
-app.get('/api/book', (req, res) => {
-  res.send('Hello World!')
+function generateUniqueId() {
+  return Math.floor(1000 + Math.random() * 9000);
+}
+
+// create application/json parser
+app.use(bodyParser.json())
+
+const books = []
+
+app.post("/api/book", (req, res)=> {
+  const newBook = {...req.body, _id: generateUniqueId()}
+  books.push(newBook)
+  res.send(newBook)
+})
+
+app.get("/api/book", (req, res)=> {
+  res.send(books)
 })
 
 // starting app on port 3000
